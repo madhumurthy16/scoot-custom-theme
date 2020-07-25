acdaf06cb39394a7b055a844bfcb79a49a7a87f8
Madhu Murthy <msmurthy@msn.com>
2020-07-24 23:12:37 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* Header file for the Scoot custom WordPress theme.
+*/
+
+?>
+
+<!DOCTYPE>
+<html <?php language_attributes() ?>>
+  <head>
+    <meta charset=<?php bloginfo('charset'); ?>>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php wp_head(); ?>
+  </head>
+
+  <body <?php body_class(); ?>>
+    <header id="site-header" role="banner">
+      <div class="header-inner section-inner">
+        <div class="header-titles-wrapper">
+          <div class="header-titles">
+            <div class="site-logo">
+              <h1><a href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'name' ); ?></a><h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+  </body>
+</html>
