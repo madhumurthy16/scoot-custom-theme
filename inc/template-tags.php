ea8d9da887cbe3fe87f2c7ae77bc1b3194304fa4
Madhu Murthy <msmurthy@msn.com>
2020-07-31 11:45:15 -0700

    add custom logo

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -11,8 +11,21 @@
  */
 
  function scoot_site_logo() {
-   if ( function_exists( 'the_custom_logo' ) ) {
-     the_custom_logo();
-   }
 
- }
+   if( has_custom_logo() ) { ?>
+
+     <h1 class="site-logo">
+       <?php the_custom_logo(); ?>
+       <span class="screen-reader-text"><?php esc_html(bloginfo( 'name' )); ?></span>
+     </h1>
+
+    <?php }
+
+   else {
+     ?>
+     <h1 class="site-title">
+       <a href="<?php echo esc_url( home_url( '/' )); ?>"><?php esc_html(bloginfo( 'name' )); ?></a>
+     </h1>
+   <?php }
+
+ } ?>
