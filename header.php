89c571eeee1fcc174d197a6c4d775e7570336960
Madhu Murthy <msmurthy@msn.com>
2020-07-31 16:32:13 -0700

    add header navigation

--- header.php
+++ header.php
@@ -26,15 +26,50 @@
 
           <div class="header-titles">
 
+            <img src="<?php echo get_template_directory_uri() . '/assets/icons/hamburger.svg' ?>" class="site-header-menu-trigger" alt="hamburger icon for mobile menu" aria-hidden="true" />
             <?php
               // Site title or logo
               scoot_site_logo();
-
             ?>
 
           </div> <!-- .header-titles -->
 
         </div> <!-- .header-titles-wrapper -->
+
+        <div class="header-navigation-wrapper">
+
+          <?php
+          if( has_nav_menu( 'primary' ) ) {
+            ?>
+
+            <nav class="primary-menu-wrapper" aria-label="<?php esc_attr_e( 'Horizontal', 'scoot' ); ?>" role="navigation">
+
+              <ul class="primary-menu reset-list-style">
+
+                <?php
+                if( has_nav_menu( 'primary' ) ) {
+
+                  wp_nav_menu(
+                    array(
+                      'container' => '',
+                      'items_wrap' => '%3$s',
+                      'theme_location' => 'primary'
+                    )
+                  );
+
+                }
+                ?>
+
+              </ul>
+            </nav> <!-- .primary-menu-wrapper -->
+
+            <?php
+          }
+          ?>
+
+          <a href="#app-download-section" class="btn-call-to-action">Get Scootin</a>
+
+        </div> <!-- .header-navigation-wrapper -->
 
       </div> <!-- .header-inner -->
     </header>
@@ -42,4 +77,3 @@
     <?php
     // Output the menu modal
     get_template_part('template-parts/modal-menu');
-    ?>
