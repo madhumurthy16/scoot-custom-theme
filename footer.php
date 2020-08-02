fa6e9d8e811d463e62bc44d9259ddefbd8e00cc3
Madhu Murthy <msmurthy@msn.com>
2020-08-01 23:37:40 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-  <a href="<?php echo esc_url( home_url( '/' )); ?>"><?php bloginfo( 'name' ); ?></a>
+  <!-- <a href="<?php echo esc_url( home_url( '/' )); ?>"><?php bloginfo( 'name' ); ?></a> -->
   <?php wp_footer(); ?>
 </footer>
 
