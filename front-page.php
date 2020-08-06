3a19cb83759e2e07539f3cf9fc9da01c62c759b6
Madhu Murthy <msmurthy@msn.com>
2020-08-05 23:18:16 -0700

    Add hero section

--- front-page.php
+++ front-page.php
@@ -2,13 +2,22 @@
 
 get_header();
 
-if( have_posts() ) {
-  while( have_posts() ) {
-    the_post();
-  }?>
-  <p><?php the_content(); ?></p>
-  
-<?php }
+?>
+
+<section class="hero-image">
+  <div class="section-inner">
+    <h1 class="hero-title">Scooter Sharing Made Simple</h1>
+    <p class="hero-text">Scoot takes the hassle out of urban mobility. Our bikes are placed in convenient
+  locations in each of our cities. Use our app to locate the nearest bike, unlock
+  it with a tap, and you’re away!</p>
+    <a href="#" class="btn-call-to-action hero-cta">Get Scootin</a>
+  </div> <!-- .section-inner -->
+  <div class="hero-pattern">
+    <img src="<?php echo get_template_directory_uri() . '/assets/patterns/right-arrow.svg' ?>" alt="" aria-hidden="true" />
+  </div>
+</section> <!-- .hero-image -->
+
+<?php
 
 get_footer();
 
