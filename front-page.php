68967a961fbacc8d981d3b50102b3d493efbbd4d
Madhu Murthy <msmurthy@msn.com>
2020-08-06 22:24:23 -0700

    update hero section

--- front-page.php
+++ front-page.php
@@ -6,11 +6,13 @@ get_header();
 
 <section class="hero-image">
   <div class="section-inner">
-    <h1 class="hero-title">Scooter Sharing Made Simple</h1>
-    <p class="hero-text">Scoot takes the hassle out of urban mobility. Our bikes are placed in convenient
-  locations in each of our cities. Use our app to locate the nearest bike, unlock
-  it with a tap, and you’re away!</p>
-    <a href="#" class="btn-call-to-action hero-cta">Get Scootin</a>
+    <div class="hero-content">
+      <h1 class="hero-title">Scooter Sharing Made Simple</h1>
+      <p class="hero-text">Scoot takes the hassle out of urban mobility. Our bikes are placed in convenient
+        locations in each of our cities. Use our app to locate the nearest bike, unlock
+        it with a tap, and you’re away!</p>
+      <a href="#" class="btn-call-to-action hero-cta">Get Scootin</a>
+    </div> <!-- .hero-content -->
   </div> <!-- .section-inner -->
   <div class="hero-pattern">
     <img src="<?php echo get_template_directory_uri() . '/assets/patterns/right-arrow.svg' ?>" alt="" aria-hidden="true" />
