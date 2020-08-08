05a54fe1d37e0ff07f24d06c9b4baecca1732691
Madhu Murthy <msmurthy@msn.com>
2020-08-07 17:29:26 -0700

    add  markup for mobile menu trigger overlay

--- front-page.php
+++ front-page.php
@@ -3,21 +3,28 @@
 get_header();
 
 ?>
+<div class="container">
+  <div class="mobile-overlay"></div>
 
-<section class="hero-image">
-  <div class="section-inner">
-    <div class="hero-content">
-      <h1 class="hero-title">Scooter Sharing Made Simple</h1>
-      <p class="hero-text">Scoot takes the hassle out of urban mobility. Our bikes are placed in convenient
-        locations in each of our cities. Use our app to locate the nearest bike, unlock
-        it with a tap, and you’re away!</p>
-      <a href="#" class="btn-call-to-action hero-cta">Get Scootin</a>
-    </div> <!-- .hero-content -->
-  </div> <!-- .section-inner -->
-  <div class="hero-pattern">
-    <img src="<?php echo get_template_directory_uri() . '/assets/patterns/right-arrow.svg' ?>" alt="" aria-hidden="true" />
-  </div>
-</section> <!-- .hero-image -->
+    <section class="hero-image">
+
+      <div class="section-inner">
+        <div class="hero-content">
+          <h1 class="hero-title">Scooter Sharing Made Simple</h1>
+          <p class="hero-text">Scoot takes the hassle out of urban mobility. Our bikes are placed in convenient
+            locations in each of our cities. Use our app to locate the nearest bike, unlock
+            it with a tap, and you’re away!</p>
+
+          <button class="btn-call-to-action hero-cta"><a href="#">Get Scootin</a></button>
+        </div> <!-- .hero-content -->
+      </div> <!-- .section-inner -->
+
+      <div class="hero-pattern">
+        <img src="<?php echo get_template_directory_uri() . '/assets/patterns/right-arrow.svg' ?>" alt="" aria-hidden="true" />
+      </div>
+    </section> <!-- .hero-image -->
+
+</div> <!-- .container -->
 
 <?php
 
