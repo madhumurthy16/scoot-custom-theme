8744294a5930020b29dbac3be526287a54fc6f07
Madhu Murthy <msmurthy@msn.com>
2020-08-19 22:32:46 -0700

    'how it works' section markup

---
+++ template-parts/home-main-content.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* Displays the main content for the front(home) page.
+*
+*/
+?>
+
+<section class="how-it-works">
+
+    <div class="accent-border"></div>
+    <img src="<?php echo get_template_directory_uri() . '/assets/icons/locate.svg' ?>" alt="" aria-hidden="true" />
+    <img src="<?php echo get_template_directory_uri() . '/assets/icons/scooter.svg' ?>" alt="" aria-hidden="true" />
+    <img src="<?php echo get_template_directory_uri() . '/assets/icons/ride.svg' ?>" alt="" aria-hidden="true" />
+
+    <div class="content-inner">
+
+      <div class="locate-scooter">
+        <h3>Locate with app</h3>
+
+        <p>Use the app to find the nearest scooter to you. We are continuously placing scooters
+        in the areas with most demand, so one should never be too far away.</p>
+      </div> <!-- .locate-scooter -->
+
+      <div class="pick-scooter">
+        <h3>Pick your scooter</h3>
+
+        <p>We show the most important info for the scooters closest to you. So you know how much
+        charge they have left and can see roughly how much it will cost.</p>
+      </div> <!-- .pick-scooter -->
+
+      <div class="ride-scooter">
+        <h3>Enjoy the ride</h3>
+
+        <p>Scan the QR code and the bike will unlock. Retract the cable lock, put on a helmet,
+        and you’re off! Always lock bikes away from walkways and accessibility ramps.</p>
+      </div> <!-- .ride-scooter -->
+
+    </div> <!-- .content-inner -->
+
+</section> <!-- .how-it-works -->
