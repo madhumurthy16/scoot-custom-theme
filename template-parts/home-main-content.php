37ac9a53f517bf9a07406edb4ba261ed4fec6add
Madhu Murthy <msmurthy@msn.com>
2020-08-21 23:29:21 -0700

    Update home-main-content.php

--- template-parts/home-main-content.php
+++ template-parts/home-main-content.php
@@ -14,7 +14,7 @@
       <div class="locate-scooter">
         <img src="<?php echo get_template_directory_uri() . '/assets/icons/locate.svg' ?>" alt="" aria-hidden="true" />
 
-        <h3>Locate with app</h3>
+        <h4>Locate with app</h4>
 
         <p>Use the app to find the nearest scooter to you. We are continuously placing scooters
         in the areas with most demand, so one should never be too far away.</p>
@@ -23,7 +23,7 @@
       <div class="pick-scooter">
         <img src="<?php echo get_template_directory_uri() . '/assets/icons/scooter.svg' ?>" alt="" aria-hidden="true" />
 
-        <h3>Pick your scooter</h3>
+        <h4>Pick your scooter</h4>
 
         <p>We show the most important info for the scooters closest to you. So you know how much
         charge they have left and can see roughly how much it will cost.</p>
@@ -32,7 +32,7 @@
       <div class="ride-scooter">
         <img src="<?php echo get_template_directory_uri() . '/assets/icons/ride.svg' ?>" alt="" aria-hidden="true" />
 
-        <h3>Enjoy the ride</h3>
+        <h4>Enjoy the ride</h4>
 
         <p>Scan the QR code and the bike will unlock. Retract the cable lock, put on a helmet,
         and you’re off! Always lock bikes away from walkways and accessibility ramps.</p>
