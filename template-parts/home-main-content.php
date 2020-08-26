5935bbf2a29571647291ee5e0a6de6e8ed6be8dc
Madhu Murthy <msmurthy@msn.com>
2020-08-25 22:21:26 -0700

    Update home-main-content.php

--- template-parts/home-main-content.php
+++ template-parts/home-main-content.php
@@ -7,37 +7,33 @@
 
 <section class="how-it-works">
 
-    <div class="accent-border"></div>
+  <div class="accent-border"></div>
 
-    <!-- <div class=""> -->
+    <div class="locate-scooter">
+      <img src="<?php echo get_template_directory_uri() . '/assets/icons/locate.svg' ?>" alt="" aria-hidden="true" />
 
-      <div class="locate-scooter">
-        <img src="<?php echo get_template_directory_uri() . '/assets/icons/locate.svg' ?>" alt="" aria-hidden="true" />
+      <h4>Locate with app</h4>
 
-        <h4>Locate with app</h4>
+      <p>Use the app to find the nearest scooter to you. We are continuously placing scooters
+      in the areas with most demand, so one should never be too far away.</p>
+    </div> <!-- .locate-scooter -->
 
-        <p>Use the app to find the nearest scooter to you. We are continuously placing scooters
-        in the areas with most demand, so one should never be too far away.</p>
-      </div> <!-- .locate-scooter -->
+    <div class="pick-scooter">
+      <img src="<?php echo get_template_directory_uri() . '/assets/icons/scooter.svg' ?>" alt="" aria-hidden="true" />
 
-      <div class="pick-scooter">
-        <img src="<?php echo get_template_directory_uri() . '/assets/icons/scooter.svg' ?>" alt="" aria-hidden="true" />
+      <h4>Pick your scooter</h4>
 
-        <h4>Pick your scooter</h4>
+      <p>We show the most important info for the scooters closest to you. So you know how much
+      charge they have left and can see roughly how much it will cost.</p>
+    </div> <!-- .pick-scooter -->
 
-        <p>We show the most important info for the scooters closest to you. So you know how much
-        charge they have left and can see roughly how much it will cost.</p>
-      </div> <!-- .pick-scooter -->
+    <div class="ride-scooter">
+      <img src="<?php echo get_template_directory_uri() . '/assets/icons/ride.svg' ?>" alt="" aria-hidden="true" />
 
-      <div class="ride-scooter">
-        <img src="<?php echo get_template_directory_uri() . '/assets/icons/ride.svg' ?>" alt="" aria-hidden="true" />
+      <h4>Enjoy the ride</h4>
 
-        <h4>Enjoy the ride</h4>
-
-        <p>Scan the QR code and the bike will unlock. Retract the cable lock, put on a helmet,
-        and you’re off! Always lock bikes away from walkways and accessibility ramps.</p>
-      </div> <!-- .ride-scooter -->
-
-    <!-- </div> --> <!-- .content-inner -->
+      <p>Scan the QR code and the bike will unlock. Retract the cable lock, put on a helmet,
+      and you’re off! Always lock bikes away from walkways and accessibility ramps.</p>
+    </div> <!-- .ride-scooter -->
 
 </section> <!-- .how-it-works -->
