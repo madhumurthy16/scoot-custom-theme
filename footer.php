4a7464c9e624dc7d7108f13b9deaa306b38081ee
Madhu Murthy <msmurthy@msn.com>
2020-08-31 22:33:23 -0700

    Add footer widget areas and credits

--- footer.php
+++ footer.php
@@ -1,7 +1,55 @@
-<footer>
-  <!-- <a href="<?php echo esc_url( home_url( '/' )); ?>"><?php bloginfo( 'name' ); ?></a> -->
-  <?php wp_footer(); ?>
+<?php
+/**
+* The template for displaying the footer.
+*/
+
+
+$has_sidebar_1 = is_active_sidebar( 'sidebar-1' );
+$has_sidebar_2 = is_active_sidebar( 'sidebar-2' );
+?>
+
+<footer id="site-footer" role="contentinfo">
+  <div class="section-inner">
+
+    <!-- Only output the container if there are elements to display -->
+
+    <?php if( $has_sidebar_1 || $has_sidebar_2 ) { ?>
+
+      <div class="footer-widgets-outer-wrapper">
+
+        <div class="footer-widgets-wrapper">
+
+          <?php if( $has_sidebar_1 ) { ?>
+
+            <div class=footer-widgets>
+              <?php dynamic_sidebar( 'sidebar-1' ); ?>
+            </div>
+
+          <?php } ?>
+
+          <?php if( $has_sidebar_2 ) { ?>
+
+            <div class="footer-widgets">
+              <?php dynamic_sidebar( 'sidebar-2' ); ?>
+            </div>
+
+          <?php } ?>
+
+        </div> <!-- .footer-widgets-wrapper -->
+
+      </div> <!-- .footer-widgets-outer-wrapper -->
+
+    <?php } ?>
+
+    <div class="footer-credits">
+      <a href="<?php echo esc_url( home_url( '/' )); ?>"><?php bloginfo( 'name' ); ?></a>
+    </div> <!-- .footer-credits -->
+
+  </div> <!-- .section-inner -->
+
 </footer>
+
+<?php wp_footer(); ?>
 
 </body>
 </html>
