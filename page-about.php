42bf14f69ca0f426b32c711d5b9872c9512dbf2a
Madhu Murthy <msmurthy@msn.com>
2020-09-18 16:19:29 -0700

    Add 'our values' section

--- page-about.php
+++ page-about.php
@@ -68,6 +68,64 @@ get_template_part( 'template-parts/page-hero');
 
     </section> <!-- .better-living -->
 
+    <section class="our-values">
+
+      <h2>Our values</h2>
+
+      <section class="tech">
+
+        <div class="tech-img-wrapper">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/our-tech.jpg' ?>" class="img-tech" aria-hidden="true" />
+          <span>01</span>
+        </div>
+
+        <h3>Our tech</h3>
+        <p>We’re using cutting edge technology to drive accessible urban transportation forward.
+           Our fully electric scooters are a joy to ride!</p>
+
+      </section> <!-- .tech -->
+
+      <section class="integrity">
+
+        <div class="integrity-img-wrapper">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/our-integrity.jpg' ?>" class="img-integrity" aria-hidden="true" />
+          <span>02</span>
+        </div>
+
+        <h3>Our integrity</h3>
+        <p>We are fully committed to deliver a great yet safe, sustainable micro-mobility experience
+           in every city we serve.</p>
+
+      </section> <!-- .integrity -->
+
+      <section class="community">
+
+        <div class="community-img-wrapper">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/our-community.jpg' ?>" class="img-community" aria-hidden="true" />
+          <span>03</span>
+        </div>
+
+        <h3>Our community</h3>
+        <p>We support every community we serve. All workers are paid a living wage based on their
+           location and are Scoot employees.</p>
+
+      </section> <!-- .community -->
+
+    </section> <!-- .our-values -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   </main> <!-- .main-content -->
 
 </div> <!-- .container -->
