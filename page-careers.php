ab3a87c9a3f05be8f0c29b4e4031b77bd83979d4
Madhu Murthy <msmurthy@msn.com>
2020-09-25 11:42:41 -0700

    add careers page header section

---
+++ page-careers.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Careers page - template file.
+*
+*/
+
+get_header();
+get_template_part('template-parts/page-hero');
+
+?>
+
+<div class="container">
+  <div class="mobile-overlay"></div>
+</div> <!-- .container -->
