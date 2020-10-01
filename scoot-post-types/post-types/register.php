4bc289cc44a6c07e97201224a30c42b775e6b6f4
Madhu Murthy <msmurthy@msn.com>
2020-09-30 22:17:40 -0700

    register job post type

---
+++ scoot-post-types/post-types/register.php
@@ -0,0 +1,24 @@
+<?php
+
+function scoot_register_job_type() {
+  $labels = array(
+    'name'          => __( 'Jobs', SCOOTDOMAIN ),
+    'singular_name' => __( 'Job', SCOOTDOMAIN ),
+    'archives'      => __( 'Job Listing', SCOOTDOMAIN ),
+    'add_new'       => __( 'Add New Job', SCOOTDOMAIN ),
+    'add_new_item'  => __( 'Add New Job', SCOOTDOMAIN ),
+    'all_items'     => __ ( 'All Jobs', SCOOTDOMAIN ),
+  );
+
+  $args = array(
+    'labels'       => $labels,
+    'public'       => true,
+    'has_archives' => 'Job Listings',
+    'rewrite'      => array( 'has_front' => true ),
+    'menu_icon'    => 'dashicons-list-view',
+    'supports'     => array( 'title', 'editor' ),
+    'show_in_rest' => true,
+  );
+
+  register_post_type( 'job', $args );
+}
