7f994df593afd60c4fd759894c62df21f474a89f
Madhu Murthy <msmurthy@msn.com>
2020-10-08 12:22:20 -0700

    Create page-location.php

---
+++ page-location.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Careers page - template file.
+*
+*/
+
+get_header();
+get_template_part('template-parts/page-hero');
+
+?>
+
+<div class="container">
+
+  <div class="mobile-overlay"></div>
+
+  <main id="main-content" role="main">
+
+    
+
+  </main> <!-- .main-content -->
+
+</div> <!-- .container -->
