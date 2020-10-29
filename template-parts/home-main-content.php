a705adae933748315d0c070d1b64a87c61a698d0
Madhu Murthy <msmurthy@msn.com>
2020-10-28 22:02:50 -0700

    Add animation class

--- template-parts/home-main-content.php
+++ template-parts/home-main-content.php
@@ -12,27 +12,27 @@
     <div class="locate-scooter">
       <img src="<?php echo get_template_directory_uri() . '/assets/icons/locate.svg' ?>" aria-hidden="true" />
 
-      <h4>Locate with app</h4>
+      <h4 class="animate-slide-up">Locate with app</h4>
 
-      <p>Use the app to find the nearest scooter to you. We are continuously placing scooters
+      <p class="animate-slide-up">Use the app to find the nearest scooter to you. We are continuously placing scooters
       in the areas with most demand, so one should never be too far away.</p>
     </div> <!-- .locate-scooter -->
 
     <div class="pick-scooter">
       <img src="<?php echo get_template_directory_uri() . '/assets/icons/scooter.svg' ?>" aria-hidden="true" />
 
-      <h4>Pick your scooter</h4>
+      <h4 class="animate-slide-up">Pick your scooter</h4>
 
-      <p>We show the most important info for the scooters closest to you. So you know how much
+      <p class="animate-slide-up">We show the most important info for the scooters closest to you. So you know how much
       charge they have left and can see roughly how much it will cost.</p>
     </div> <!-- .pick-scooter -->
 
     <div class="ride-scooter">
       <img src="<?php echo get_template_directory_uri() . '/assets/icons/ride.svg' ?>" aria-hidden="true" />
 
-      <h4>Enjoy the ride</h4>
+      <h4 class="animate-slide-up">Enjoy the ride</h4>
 
-      <p>Scan the QR code and the bike will unlock. Retract the cable lock, put on a helmet,
+      <p class="animate-slide-up">Scan the QR code and the bike will unlock. Retract the cable lock, put on a helmet,
       and you’re off! Always lock bikes away from walkways and accessibility ramps.</p>
     </div> <!-- .ride-scooter -->
 
@@ -55,13 +55,13 @@
 
   <div class="telemetry-desc">
 
-    <h2>Easy to use riding telemetry</h2>
+    <h2 class="animate-slide-up">Easy to use riding telemetry</h2>
 
-    <p>The Scoot app is available with riding telemetry. This means it can show you your
+    <p class="animate-slide-up">The Scoot app is available with riding telemetry. This means it can show you your
       average speed, how long you've been using the scooter, your traveling distance,
       and many more things all in an easy to use app.</p>
 
-    <a href="https://scoot.local/about/" class="btn-call-to-action telemetry-cta">Learn More</a>
+    <a href="https://scoot.local/about/" class="btn-call-to-action telemetry-cta animate-slide-up">Learn More</a>
 
   </div> <!-- .telemetry-desc -->
 
@@ -86,13 +86,13 @@
 
   <div class="cities-desc">
 
-    <h2>Coming to a city near you</h2>
+    <h2 class="animate-slide-up">Coming to a city near you</h2>
 
-    <p>Scoot is available in 4 major cities so far. We’re expanding rapidly, so be sure
+    <p class="animate-slide-up">Scoot is available in 4 major cities so far. We’re expanding rapidly, so be sure
        to let us know if you want to see us in your hometown. We’re aiming to let our
        scooters loose on 23 cities over the coming year.</p>
 
-    <a href="https://scoot.local/location/" class="btn-call-to-action cities-cta">Learn More</a>
+    <a href="https://scoot.local/location/" class="btn-call-to-action cities-cta animate-slide-up">Learn More</a>
 
   </div> <!-- .cities-desc -->
 
@@ -117,13 +117,13 @@
 
   <div class="payments-desc">
 
-    <h2>Zero hassle payments</h2>
+    <h2 class="animate-slide-up">Zero hassle payments</h2>
 
-    <p>Our payment is as easy as one two three. We accept most credit cards and debit cards.
+    <p class="animate-slide-up">Our payment is as easy as one two three. We accept most credit cards and debit cards.
   You can also link your PayPal account inside the app. Need to pay later? No worries!
   You can defer payment for up to a month.</p>
 
-    <a href="https://scoot.local/about/" class="btn-call-to-action payments-cta">Learn More</a>
+    <a href="https://scoot.local/about/" class="btn-call-to-action payments-cta animate-slide-up">Learn More</a>
 
   </div> <!-- .payments-desc -->
 
