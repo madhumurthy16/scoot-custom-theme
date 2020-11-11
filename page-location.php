3559aaced45c36d9eba29253495b81ee9e410405
Madhu Murthy <msmurthy@msn.com>
2020-11-10 21:24:03 -0800

    add class name for animation

--- page-location.php
+++ page-location.php
@@ -53,7 +53,7 @@ get_template_part('template-parts/page-hero');
 
     <section class="message-us section-inner content-inner">
 
-      <h2>Your city not listed?</h2>
+      <h2 class="right-on-scroll">Your city not listed?</h2>
       <p>If you’d like to see Scoot in your hometown, be sure to let us know. We track
          requests and plan launches based on demand. Feel free to message us by clicking
          the link or messaging us on social.</p>
